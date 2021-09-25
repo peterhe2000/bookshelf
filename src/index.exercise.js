432e4487f66e5a89791d0c38289d171d11d16be3
peterhe <peter.he@sandstone.com.au>
2021-09-25 17:03:39 +1000

    Render react app 1 done.

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,3 +1,23 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
+
+const App = () => {
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => alert('login clicked')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => alert('register clicked')}>Register</button>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 // 🐨 you'll need to import React and ReactDOM up here
 
 // 🐨 you'll also need to import the Logo component from './components/logo'

e9def4e99733170c2dbf74c40a28085519aa05f8
peterhe <peter.he@sandstone.com.au>
2021-10-02 19:02:10 +1000

    04 Auth. 01-extra2 done. Use custom hooker to handle diff login state

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,6 +6,9 @@ import * as auth from 'auth-provider'
 import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {useAsync} from './utils/hooks'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from './styles/colors'
 
 async function getUser() {
   let user = null
@@ -20,31 +23,60 @@ async function getUser() {
 }
 
 function App() {
-  // 🐨 useState for the user
-  const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData,
+  } = useAsync()
 
   React.useEffect(() => {
-    getUser().then(u => setUser(u))
-  }, [])
+    run(getUser())
+  }, [run])
 
   // 🐨 create a login function that calls auth.login then sets the user
-  const login = form => auth.login(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(u => setData(u))
   // 🐨 create a registration function that does the same as login except for register
-  const register = form => auth.register(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setData(u))
   // 🐨 create a logout function that calls auth.logout() and sets the user to null
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setData(null)
   }
 
   // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
   // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  if (isError) {
+    ;<div
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  }
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}

58c15a897b2ca4ba94a86b786a2e17a2f83021d2
peterhe <peter.he@sandstone.com.au>
2021-10-02 19:08:32 +1000

    04 Auth. 01-extra3 done. Handle token expired case by delete user token and reload the page.

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,3 +1,4 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(endpoint, {token, headers: customHeaders, ...customConfig}) {
@@ -11,6 +12,12 @@ function client(endpoint, {token, headers: customHeaders, ...customConfig}) {
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     const data = await response.json()
+    if (response.status === 401) {
+      await auth.logout()
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
+
     if (response.ok) {
       return data
     } else {

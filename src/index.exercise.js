b7b1d247c94e7942887e2dab0e46a72e61b6091a
Peterhe <peterhe2000@gmail.com>
2022-04-18 10:42:31 +1000

    Added ReactQueryConfigProvider and queryConfig for error retry.

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,7 +3,25 @@ import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query'
+
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false, // refetch when window focus
+    retry(failureCount, error) {
+      if (error.status === 404) {
+        return false
+      } else return failureCount < 2;
+    },
+  },
+}
 
 loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>,
+    document.getElementById('root'),
+  )
 })
